4a25c8742f0a049b038f252e2764f241cd26f82e
Julio Cardoza <julio_jr3@hotmail.com>
2016-06-01 16:08:03 -0400

    modified settings file

--- smart_driver/smart_driver/settings.py
+++ smart_driver/smart_driver/settings.py
@@ -49,11 +49,17 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    #'django.contrib.auth.middleware.RemoteUserMiddleware',
+    #'django.contrib.auth.middleware.PersistentRemoteUserMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
 )
+
+# AUTHENTICATION_BACKENDS = [
+#     'django.contrib.auth.backends.RemoteUserBackend',
+# ]
 
 ROOT_URLCONF = 'smart_driver.urls'
 
