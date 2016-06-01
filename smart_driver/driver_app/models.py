b9803354f533f413da24835528b6f36f4960ab2a
Kathryn Jackson <kathryn.jackson.nc@gmail.com>
2016-06-01 13:28:33 -0400

    less model attr, null true for dependent calculations

--- smart_driver/driver_app/models.py
+++ smart_driver/driver_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 # from decimal import *
 # import dateutil.parser
 # from dateutil.tz import *
@@ -10,20 +11,14 @@ class Ride(models.Model):
     day_statement = models.ForeignKey('DayStatement', on_delete=models.CASCADE)
     trip_id = models.CharField(max_length=50, primary_key=True)
     status = models.CharField(max_length=15)
-    fare = models.DecimalField(max_digits=8, decimal_places=2)
-    uber_fee = models.DecimalField(max_digits=7, decimal_places=2)
     total_earned = models.DecimalField(max_digits=8, decimal_places=2)
     surge = models.DecimalField(max_digits=7, decimal_places=2, null=True)
-    currency_code = models.CharField(max_length=5)
     distance = models.DecimalField(max_digits=7, decimal_places=2)
     date = models.DateTimeField()
     request_at = models.DateTimeField()
     begintrip_at = models.DateTimeField()
     dropoff_at = models.DateTimeField()
     duration = models.DurationField()
-    driver_feedback = models.PositiveSmallIntegerField()
-    driver_rating = models.DecimalField(max_digits=3, decimal_places=2)
-    ride_type = models.CharField(max_length=25)
 
 
 class DayStatement(models.Model):
@@ -33,9 +28,9 @@ class DayStatement(models.Model):
     total_earned = models.DecimalField(max_digits=8, decimal_places=2)
     date = models.DateTimeField()
     weekday = models.IntegerField(choices=WEEKDAY_CHOICES, default=0)
-    time_worked = models.DurationField()
+    time_worked = models.DurationField(null=True)
     rate_per_ride = models.DecimalField(max_digits=8, decimal_places=2)
-    rate_per_hour = models.DecimalField(max_digits=8, decimal_places=2)
+    rate_per_hour = models.DecimalField(max_digits=8, decimal_places=2, null=True)
     week_statement = models.ForeignKey('WeekStatement', on_delete=models.CASCADE)
 
 
@@ -45,12 +40,8 @@ class WeekStatement(models.Model):
     starting_at = models.DateTimeField()
     ending_at = models.DateTimeField()
     total_earned = models.DecimalField(max_digits=8, decimal_places=2)
-    total_fare = models.DecimalField(max_digits=8, decimal_places=2)
-    total_uber_fee = models.DecimalField(max_digits=8, decimal_places=2)
     total_surge = models.DecimalField(max_digits=8, decimal_places=2)
-    trip_count = models.PositiveSmallIntegerField()
-    total_distance = models.DecimalField(max_digits=8, decimal_places=2)
-    city = models.SlugField(max_length=100)
+    total_distance = models.DecimalField(max_digits=8, decimal_places=2, null=True)
 
 
 class Driver(models.Model):
