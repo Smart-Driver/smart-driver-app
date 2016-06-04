a1dc1211209537401916230b48369ac6524e15db
Julio Cardoza <julio_jr3@hotmail.com>
2016-06-04 18:09:37 -0400

    messing arround with the data_table.js and views files

--- smart_driver/driver_app/static/driver_app/data_table.js
+++ smart_driver/driver_app/static/driver_app/data_table.js
@@ -1,4 +1,6 @@
 
 $(document).ready(function(){
-    $('#table_id').DataTable();
+    $('#table_id').DataTable({
+        // "ajax": '/api/day_statements'
+    });
 });
--- smart_driver/driver_app/views.py
+++ smart_driver/driver_app/views.py
@@ -11,23 +11,24 @@ from .models import Ride, DayStatement, WeekStatement, Driver
 
 
 class RideViewSet(viewsets.ModelViewSet):
-     queryset = Ride.objects.all()
-     serializer_class = RideSerializer
+    queryset = Ride.objects.all()
+    serializer_class = RideSerializer
 
 
 class DayStatementViewSet(viewsets.ModelViewSet):
-     queryset = DayStatement.objects.all()
-     serializer_class = DayStatementSerializer
+    #DayStatement.objects.filter(driver=driver).order_by('-date')
+    queryset = DayStatement.objects.all()
+    serializer_class = DayStatementSerializer
 
 
 class WeekStatementViewSet(viewsets.ModelViewSet):
-     queryset = WeekStatement.objects.all()
-     serializer_class = WeekStatementSerializer
+    queryset = WeekStatement.objects.all()
+    serializer_class = WeekStatementSerializer
 
 
 class DriverViewSet(viewsets.ModelViewSet):
-     queryset = Driver.objects.all()
-     serializer_class = DriverSerializer
+    queryset = Driver.objects.all()
+    serializer_class = DriverSerializer
 
 
 def home(request):
