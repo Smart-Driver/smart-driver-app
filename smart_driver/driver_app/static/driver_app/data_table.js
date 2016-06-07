73f9d48fac8ba32bb955686c8815edac86ddccd1
Kathryn Jackson <kathryn.jackson.nc@gmail.com>
2016-06-07 14:58:10 -0400

    filtered table by driver. created tablecreate functions

--- smart_driver/driver_app/static/driver_app/data_table.js
+++ smart_driver/driver_app/static/driver_app/data_table.js
@@ -1,21 +1,18 @@
-
 var a = document.getElementById('data_type').selectedIndex
 var table_header = $('#table_id').find('tr')
+var driverID = $('#driver_id').val()
 
-
-$(document).ready(function(){
-
-  if (a == 0){
+function createDayTable() {
     table_header.append(
-      $('<th>').text('Date'),
-      $('<th>').text('Weekday'),
-      $('<th>').text('Total Earned'),
-      $('<th>').text('Time Worked'),
-      $('<th>').text('Rate/Hr'),
-      $('<th>').text('Total Rides'),
-      $('<th>').text('Rate/Ride')
+        $('<th>').text('Date'),
+        $('<th>').text('Weekday'),
+        $('<th>').text('Total Earned'),
+        $('<th>').text('Time Worked'),
+        $('<th>').text('Rate/Hr'),
+        $('<th>').text('Total Rides'),
+        $('<th>').text('Rate/Ride')
     );
-    $.get('/api/day_statements',
+    $.get('/api/day_statements/?driver=' + driverID,
         function(data) {
             $('#table_id').DataTable({
                 data: data,
@@ -27,12 +24,49 @@ $(document).ready(function(){
                     {data: 'rate_per_hour'},
                     {data: 'total_rides'},
                     {data: 'rate_per_ride'}
-                ]
+                ],
+                aaSorting: [[0, 'desc']]
             });
         }
     );
+}
 
-  };
+function destroyTable() {
+    table_header.find('th').remove();
+    $('#table_id').DataTable.destroy();
+}
+
+function drawWeekTable() {
+    table_header.append(
+        $('<th>').text('Starting'),
+        $('<th>').text('Ending'),
+        $('<th>').text('Total Earned'),
+        $('<th>').text('Avg Rate/Day'),
+        $('<th>').text('Avg Rate/Hr'),
+        $('<th>').text('Total Rides'),
+        $('<th>').text('Rate/Ride')
+    );
+    $.get('/api/week_statements/?driver=' + driverID,
+       function(data) {
+           $('#table_id').DataTable({
+               data: data,
+               columns: [
+                   {data: 'starting_at'},
+                   {data: 'ending_at'},
+                   {data: 'total_earned'},
+                   {data: 'rate_per_day'},
+                   {data: 'rate_per_hour'},
+                   {data: 'total_rides'},
+                   {data: 'rate_per_ride'}
+               ],
+                aaSorting: [[0, 'desc']]
+            });
+        }
+    );
+}
+
+$(document).ready(function() {
+    createDayTable();
 
 // $('#table_id').change(function() {
 //     var a = selected
