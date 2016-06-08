688a078c7a4d9234aee75a8570186a30f5b54458
Kathryn Jackson <kathryn.jackson.nc@gmail.com>
2016-06-08 15:32:35 -0400

    filter table by click on weekday graph

--- smart_driver/driver_app/static/driver_app/data_table.js
+++ smart_driver/driver_app/static/driver_app/data_table.js
@@ -1,7 +1,47 @@
 var table_header = $('#table_id').find('tr')
 var driverID = $('#driver_id').val()
+var m = null
+var w = null
 
-
+function drawDayTable(month = m, weekday = w) {
+    m = month
+    w = weekday
+    var url = '/api/day_statements/?driver=' + driverID
+    if (month) {
+        url += '&month=' + month
+    }
+    if (weekday) {
+        url += '&weekday=' + weekday
+    }
+    table_header.append(
+        $('<th>').text('Date'),
+        $('<th>').text('Weekday'),
+        $('<th>').text('Total Earned'),
+        $('<th>').text('Time Worked'),
+        $('<th>').text('Rate/Hr'),
+        $('<th>').text('Total Rides'),
+        $('<th>').text('Rate/Ride')
+    );
+    $.get(url,
+        function(data) {
+            $('#table_id').DataTable({
+                "pageLength": 30,
+                "bLengthChange": false,
+                data: data,
+                columns: [
+                    {data: 'date'},
+                    {data: 'weekday'},
+                    {data: 'total_earned'},
+                    {data: 'time_worked'},
+                    {data: 'rate_per_hour'},
+                    {data: 'total_rides'},
+                    {data: 'rate_per_ride'}
+                ],
+                aaSorting: [[0, 'desc']]
+            });
+        }
+    );
+}
 
 function destroyTable() {
     $('#table_id').DataTable().destroy();
@@ -26,6 +66,8 @@ function drawWeekTable(month = m) {
     $.get(url,
        function(data) {
            $('#table_id').DataTable({
+               "pageLength": 30,
+               "bLengthChange": false,
                data: data,
                columns: [
                    {data: 'starting_at'},
@@ -42,75 +84,44 @@ function drawWeekTable(month = m) {
     );
 }
 
-var a = document.getElementById('data_type').selectedIndex
+var filterSelection = document.getElementById('data_type').selectedIndex
+
+function filterTable(m, w) {
+    destroyTable();
+    if (filterSelection == 0) {
+        drawDayTable(m, w);
+    }
+    else {
+        drawWeekTable(m);
+    }
+}
 
 $(document).ready(function() {
     drawDayTable();
 
     $('#data_type').change(function() {
-        a = document.getElementById('data_type').selectedIndex;
-        if (a == 0) {
-            destroyTable();
-            drawDayTable();
-        }
-        else {
-            destroyTable();
-            drawWeekTable();
-        }
+        filterSelection = document.getElementById('data_type').selectedIndex;
+        filterTable(m, w);
     });
 
 });
+
+function onChart1Created() {
+    d3.select('#chart1 svg')
+        .selectAll('.discreteBar')
+        .on('click',
+            function (d) {
+                document.getElementById('data_type').selectedIndex = 0
+                filterSelection = 0
+                filterTable(m, d['label'])
+            });
+}
 
 function onChart2Created() {
     d3.select('#chart2 svg')
         .selectAll('.discreteBar')
         .on('click',
             function (d) {
-                destroyTable();
-                if (a == 0){
-                    drawDayTable(d['label']);
-                }
-                else {
-                    drawWeekTable(d['label']);
-                }
-
+                filterTable(d['label'])
             });
-}
-
-var m = None
-
-function drawDayTable(month = m) {
-    m = month
-    var url = '/api/day_statements/?driver=' + driverID
-    if (month) {
-        url += '&month=' + month
-    }
-    table_header.append(
-        $('<th>').text('Date'),
-        $('<th>').text('Weekday'),
-        $('<th>').text('Total Earned'),
-        $('<th>').text('Time Worked'),
-        $('<th>').text('Rate/Hr'),
-        $('<th>').text('Total Rides'),
-        $('<th>').text('Rate/Ride')
-    );
-    $.get(url,
-        function(data) {
-            $('#table_id').DataTable({
-                "pageLength": 31,
-                "bLengthChange": false,
-                data: data,
-                columns: [
-                    {data: 'date'},
-                    {data: 'weekday'},
-                    {data: 'total_earned'},
-                    {data: 'time_worked'},
-                    {data: 'rate_per_hour'},
-                    {data: 'total_rides'},
-                    {data: 'rate_per_ride'}
-                ],
-                aaSorting: [[0, 'desc']]
-            });
-        }
-    );
 }
