1df5a07bcc0c6ef74d84d22a04f8b2b5aa93309b
oakes <oakesonline@gmail.com>
2016-06-10 13:44:14 -0400

    Heroku test

--- smart_driver/driver_app/views.py
+++ smart_driver/driver_app/views.py
@@ -97,7 +97,7 @@ def home(request):
 
         else:
             print('login_failed')
-            context['fail'] = True
+            context['fail'] = login_response.status_code
 
     return render(request, "driver_app/home.html", context)
 
