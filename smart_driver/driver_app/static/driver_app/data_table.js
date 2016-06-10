4bee07d6c9e01a394ff636500a9e102d8e4ac45b
Kathryn Jackson <kathryn.jackson.nc@gmail.com>
2016-06-10 10:16:45 -0400

    weekday graph changes with month filtering

--- smart_driver/driver_app/static/driver_app/data_table.js
+++ smart_driver/driver_app/static/driver_app/data_table.js
@@ -1,12 +1,10 @@
-
-
 var table_header = $('#table_id').find('tr')
 var driverID = $('#driver_id').val()
 var m = null
-var w = null
 var sum_total_earned = 0;
 var avg_per_unit = 0;
 var avg_per_hour = 0;
+
 
 // GET AVERAGES FOR COLUMNS IN DATATABLE
 function getStats(data) {
@@ -29,9 +27,9 @@ function getStats(data) {
 };
 
 // CREATE DATATABLE USING API CALL TO GET DAYSTATEMENTS
-function drawDayTable(month = m, weekday = w) {
+function drawDayTable(month = m, weekday) {
     m = month
-    w = weekday
+    // w = weekday
     var url = '/api/day_statements/?driver=' + driverID
     if (month) {
         url += '&month=' + month
@@ -71,9 +69,13 @@ function drawDayTable(month = m, weekday = w) {
             $('#avg_per_unit h3').html('Avg Daily Rate');
             $('#avg_per_unit p').html(avg_per_unit);
             $('#total_earned p').html(sum_total_earned);
+
+            if (weekday == null) {
+                drawWeekGraph(formatGraphData(dataTable.data()));
+            };
         });
         $("#time_worked").qtip();
-}
+};
 
 function destroyTable() {
     $('#table_id').DataTable().destroy();
@@ -91,8 +93,8 @@ function drawWeekTable(month = m) {
         $('<th>').text('Starting'),
         $('<th>').text('Ending'),
         $('<th>').text('Total Earned'),
-        $('<th>').text('Avg Rate/Day'),
-        $('<th>').text('Avg Rate/Hr'),
+        $('<th>').text('Rate/Day'),
+        $('<th>').text('Rate/Hr'),
         $('<th>').text('Total Rides'),
         $('<th>').text('Rate/Ride')
     );
@@ -112,7 +114,7 @@ function drawWeekTable(month = m) {
                     {data: 'rate_per_ride'}
                 ],
                 aaSorting: [[0, 'desc']]
-            });
+        });
             getStats(dataTable.data());
             $('#avg_per_hour p').html(avg_per_hour);
             $('#avg_per_unit h3').html('Avg Weekly Rate');
@@ -120,10 +122,10 @@ function drawWeekTable(month = m) {
             $('#total_earned p').html(sum_total_earned);
         }
     );
-}
+};
 
 // FIND DAILY/WEEKLY OPTION SELECTED IN DROPDOWN (0=DAILY, 1=WEEKLY)
-var filterSelection = document.getElementById('data_type').selectedIndex
+var filterSelection = document.getElementById('data_type').selectedIndex;
 
 // DETERMINE DAILY/WEEKLY OPTION SELECTED AND REDRAW FILTERED TABLE
 function filterTable(m, w) {
@@ -134,7 +136,7 @@ function filterTable(m, w) {
     else {
         drawWeekTable(m);
     }
-}
+};
 
 // CREATE INITIAL TABLE OF ALL DAYSTATEMENT, HOOK-UP DAILY/WEEKLY DROPDOWN
 $(document).ready(function() {
@@ -158,7 +160,7 @@ function onChart1Created() {
                 filterTable(m, d['label'])
             }
         );
-}
+};
 
 // LINK MONTH CHART (ONCE IT'S LOADED) TO REDRAWING FILTERED TABLE
 function onChart2Created() {
@@ -169,4 +171,66 @@ function onChart2Created() {
                 filterTable(d['label'])
             }
         );
-}
+};
+
+function formatGraphData(data) {
+    graphData = [{key:"Avg Hourly Rate By Weekday", values:[]}];
+
+    var mondaySum = 0
+    var mondayCount = 0
+    var tuesdaySum = 0
+    var tuesdayCount = 0
+    var wednesdaySum = 0
+    var wednesdayCount = 0
+    var thursdaySum = 0
+    var thursdayCount = 0
+    var fridaySum = 0
+    var fridayCount = 0
+    var saturdaySum = 0
+    var saturdayCount = 0
+    var sundaySum = 0
+    var sundayCount = 0
+
+    for (i in data) {
+        if (data[i].weekday == "Monday") {
+            mondaySum += Number(data[i].rate_per_hour.replace("$", ""))
+            ++mondayCount
+        }
+        else if (data[i].weekday == "Tuesday") {
+            tuesdaySum += Number(data[i].rate_per_hour.replace("$", ""))
+            ++tuesdayCount
+        }
+        else if (data[i].weekday == "Wednesday") {
+            wednesdaySum += Number(data[i].rate_per_hour.replace("$", ""))
+            ++wednesdayCount
+        }
+        else if (data[i].weekday == "Thursday") {
+            thursdaySum += Number(data[i].rate_per_hour.replace("$", ""))
+            ++thursdayCount
+        }
+        else if (data[i].weekday == "Friday") {
+            fridaySum += Number(data[i].rate_per_hour.replace("$", ""))
+            ++fridayCount
+        }
+        else if (data[i].weekday == "Saturday") {
+            saturdaySum += Number(data[i].rate_per_hour.replace("$", ""))
+            ++saturdayCount
+        }
+        else if (data[i].weekday == "Sunday") {
+            sundaySum += Number(data[i].rate_per_hour.replace("$", ""))
+            ++sundayCount
+        }
+    };
+
+    graphData[0]["values"].push(
+        {"label": "Monday", "value": mondaySum / mondayCount},
+        {"label": "Tuesday", "value": tuesdaySum / tuesdayCount},
+        {"label": "Wednesday", "value": wednesdaySum / wednesdayCount},
+        {"label": "Thursday", "value": thursdaySum / thursdayCount},
+        {"label": "Friday", "value": fridaySum / fridayCount},
+        {"label": "Saturday", "value": saturdaySum / saturdayCount},
+        {"label": "Sunday", "value": sundaySum / sundayCount}
+    );
+
+    return graphData
+};
